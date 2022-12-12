b9e71a5d687eb8af49f56abd6dbf3189c9e4d13d
rakeshirukula <58940709+rakeshirukula@users.noreply.github.com>
2022-12-12 17:08:20 +0530

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -48,6 +48,7 @@ public class Hook extends BaseUtil{
             System.out.println(scenario.getName());
         }
         System.out.println("Closing the browser : MOCK");
+        base.Driver.quit();
         
     }
 
